ce1a121c8f46b8a91a1fcf2a9716a155ece4efa3
meier-th <steinmann.thms@gmail.com>
2020-02-09 11:06:07 +0300

    Requests bugs are fixed

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,8 @@ import { AccountService } from '../services/account.service';
 import { UserService } from '../services/user.service';
 import { User } from '../entities/user';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RequestService } from '../services/request.service';
+import { MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profile',
@@ -17,10 +19,13 @@ export class ProfileComponent implements OnInit {
   myName: string;
   areContactsPrivate: boolean;
   avatar: any;
+  opened: boolean = false;
+  requestMessage: string;
+  requestExists: boolean = false;
 
   constructor(private userService: UserService,
     private route: ActivatedRoute, private authService: AccountService,
-    private router: Router) { }
+    private router: Router, private requestService: RequestService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -36,12 +41,25 @@ export class ProfileComponent implements OnInit {
 
     this.userService.getContacts(this.username).subscribe(data => {
       this.user.contacts = data;
-      console.log(data);
     });
+    this.requestService.loadOutgoingRequests().subscribe(requests => {
+      requests.forEach(req => {
+        if (req.to == this.username)
+          this.requestExists = true;
+      });
+    });
+    if (this.username === this.myName)
+      this.requestExists = true;
   }
 
   openEditor() {
     this.router.navigateByUrl('/mutable-profile/'+this.username);
+  }
+
+  sendRequest() {
+      this.requestService.sendRequest(this.username, this.requestMessage).subscribe(response => {
+        this.opened = false;
+      });
   }
 
 }
