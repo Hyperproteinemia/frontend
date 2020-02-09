d51591add9c2a5f6a2fa0aef8f50ec9334cc321d
meier-th <steinmann.thms@gmail.com>
2020-02-09 07:53:59 +0300

    merge

--- src/app/services/user-contacts.service.ts
+++ src/app/services/user-contacts.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { User } from '../entities/user';
+import { ContactType, Contact } from '../entities/contact';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,97 @@ import { Injectable } from '@angular/core';
 export class UserContactsService {
 
   constructor() { }
+
+  public contains(user: User, type: ContactType) : boolean {
+    user.contacts.forEach(user => {
+      if (user.type == type)
+        return true;
+    });
+    return false;
+  }
+
+  public getTelegram(user: User): string {
+    user.contacts.forEach(contact => {
+        if (contact.type == ContactType.TELEGRAM)
+            return contact.value;
+    });
+    return '';
+}
+
+public getWhatsApp(user: User): string {
+  user.contacts.forEach(contact => {
+        if (contact.type == ContactType.WHATSAPP)
+            return contact.value;
+    });
+    return '';
+}
+
+public getMobileNumber(user: User) : string {
+  user.contacts.forEach(contact => {
+        if (contact.type == ContactType.MOBILE)
+            return contact.value;
+    });
+    return '';
+}
+
+public getInstagram(user: User): string {
+  user.contacts.forEach(contact => {
+        if (contact.type == ContactType.INSTAGRAM)
+            return contact.value;
+    });
+    return '';
+}
+
+public setTelegram(user: User, number: string) {
+  if (this.contains(user, ContactType.TELEGRAM)) {
+  user.contacts.forEach(contact => {
+        if (contact.type == ContactType.TELEGRAM) {
+            contact.value = number;
+            return;
+        }
+    });
+  } else {
+    user.contacts.push({type: ContactType.TELEGRAM, value: number});
+  }
+}
+
+public setWhatsApp(user: User, number: string) {
+  if (this.contains(user, ContactType.WHATSAPP)) {
+  user.contacts.forEach(contact => {
+        if (contact.type == ContactType.WHATSAPP) {
+            contact.value = number;
+            return;
+        }
+    });
+  } else {
+    user.contacts.push({type: ContactType.WHATSAPP, value: number});
+  }
+}
+
+public setPhoneNumber(user: User, number: string) {
+  if (this.contains(user, ContactType.MOBILE)) {
+  user.contacts.forEach(contact => {
+        if (contact.type == ContactType.MOBILE) {
+            contact.value = number;
+            return;
+        }
+    });
+  } else {
+    user.contacts.push({type: ContactType.MOBILE, value: number});
+  }
+}
+
+public setInstagram(user: User, number: string) {
+  if (this.contains(user, ContactType.INSTAGRAM)) {
+  user.contacts.forEach(contact => {
+        if (contact.type == ContactType.INSTAGRAM) {
+            contact.value = number;
+            return;
+        }
+    });
+  } else {
+    user.contacts.push({type: ContactType.INSTAGRAM, value: number});
+  }
+}
+
 }
